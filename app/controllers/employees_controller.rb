748057ebfbc8e2298493bb83ade036044910a36a
BenMiriello <benmiriello@gmail.com>
2019-12-17 17:09:44 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  
+
   def index 
     @employees = Employee.all
   end 
@@ -8,4 +8,22 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
   end 
 
+  def new
+    flash
+    @employee = Employee.new
+  end
+
+  def create 
+    employee_params = params.require(:employee).permit(:first_name, :last_name, :alias, :office, :dog_id, :img_url, :title)
+    @employee = Employee.create(employee_params)
+
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path
+    end
+  end 
+
 end
+
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ApplicationRecord
   belongs_to :dog
+
+  validates :alias, uniqueness: { message: "Alias already exists. Please enter a unique alias."}
+  validates :title, uniqueness: { message: "Title already exists. Please enter a unique title."}
+
 end
+
